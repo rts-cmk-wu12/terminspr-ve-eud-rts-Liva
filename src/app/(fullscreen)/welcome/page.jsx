3deb94f6de96bea453e43487ee780cad610b0eb3
rts-Liva <U2451012@rts-365.dk>
2025-09-01 15:54:13 +0200

    welcome page

---
+++ src/app/(fullscreen)/welcome/page.jsx
@@ -0,0 +1,14 @@
+import '@/scss/components/splash-image.scss';
+import '@/scss/pages/welcome.scss';
+
+function WelcomePage() {
+    return (
+        <div className="wrapper">
+            <section className="logo">
+                <h1 className="logo__heading">landrup <span className="logo__heading logo__heading--bottom">dans</span></h1>
+            </section>
+        </div>
+    );
+}
+
+export default WelcomePage;
