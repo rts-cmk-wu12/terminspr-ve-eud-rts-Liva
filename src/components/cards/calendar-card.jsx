26d0f207d03adad63b01e3a13bba94f1674fe5e7
rts-Liva <U2451012@rts-365.dk>
2025-09-03 11:19:17 +0200

    fix link path

--- src/components/cards/calendar-card.jsx
+++ src/components/cards/calendar-card.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-function CalendarCard({ activity }) {
+function CalendarCard({ activity, linkUrl }) {
     return (
         <li className="calendar__card">
-            <Link href={`/aktivitet/${activity.id}`}>
-                <h2 className="calendar__card__heading">{activity.name || activity.activity}</h2>
+            <Link href={linkUrl}>
+                <h2 className="calendar__card__heading">{activity.name}</h2>
                 <p className="calendar__card__text">{activity.weekday} {activity.time}</p>
             </Link>
         </li>
