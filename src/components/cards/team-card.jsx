e0f8caf1026e5299e041d34d07b4f00ced16b8b0
rts-Liva <U2451012@rts-365.dk>
2025-09-03 11:19:46 +0200

    team overview page

---
+++ src/components/cards/team-card.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+import useFetch from "@/hooks/use-fetch";
+
+function TeamCard({ id }) {
+    const { data, error, loading } = useFetch(`activities/${id}`);
+
+    return (
+        <section className="team">
+            {loading && <p>Indlæser...</p>}
+            {error && <p>Aktivitet kunne ikke findes</p>}
+            {data && (
+                <>
+                    <h1 className="team__heading">{data.name}</h1>
+                    <ul>
+                        {data.users.length ? (
+                            data.users.map(user => (
+                                <li key={user.id}>{user.firstname} {user.lastname}</li>
+                            ))
+                        ) : <p>Ingen tilmeldte fundet</p>}
+                    </ul>
+                </>
+            )}
+        </section>
+    );
+}
+
+export default TeamCard;
--- src/middleware.js
+++ src/middleware.js
@@ -19,15 +19,34 @@ export default async function middleware(request) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
+    if (pathname.includes('/hold-oversigt')) {
+        const user_id = request.cookies.get('user_id');
+        const access_token = request.cookies.get('access_token');
+        const response = await fetch(`http://localhost:4000/api/v1/users/${user_id?.value}`, {
+            headers: {
+                Authorization: `Bearer ${access_token?.value}`
+            }
+        });
+
+        const user = await response.json();
+
+        if (user.role !== 'instructor') {
+            return NextResponse.redirect(new URL('/', request.url));
+        };
+
+        return;
+    };
+
     return;
 }
 
 export const config = {
     matcher: [
         '/',
-        '/activity/:path*',
-        '/search/:path*',
-        '/calendar/:path*',
+        '/aktivitet/:path*',
+        '/søg/:path*',
+        '/kalender/:path*',
+        '/hold-oversigt/:path*',
         '/login/:path*',
     ]
 };
