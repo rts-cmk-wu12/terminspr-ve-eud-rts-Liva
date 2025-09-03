47b00a48e4346d8af87f272cc9532b6538d362ef
rts-Liva <U2451012@rts-365.dk>
2025-09-03 11:19:30 +0200

    change fetch endpoint

--- src/components/activity-overview.jsx
+++ src/components/activity-overview.jsx
@@ -1,19 +1,14 @@
-import authFetch from "@/utils/auth-fetch";
 import CalendarCard from "./cards/calendar-card";
 
-async function ActivityOverview({ id, userId }) {
-    const roster = await authFetch(`users/${userId}/roster/${id}`, 'kalender');
-
-    if (!roster.length) return;
+async function ActivityOverview({ activity }) {
+    if (!activity.users.length) return;
 
     return (
         <ul className="calendar">
-            {roster?.map(activity => (
-                <CalendarCard
-                    key={`instructor ${activity.id}`}
-                    activity={activity}
-                    linkUrl={`/hold-oversigt/${activity.id}`} />
-            ))}
+            <CalendarCard
+                key={activity.id}
+                activity={activity}
+                linkUrl={`/hold-oversigt/${activity.id}`} />
         </ul>
     );
 }
