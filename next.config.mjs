5e47963837d8f291633fc531c1cec138f062bac7
rts-Liva <U2451012@rts-365.dk>
2025-09-04 14:34:26 +0200

    image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [new URL('http://localhost:4000/file-bucket/**')],
+        remotePatterns: [
+            new URL('http://localhost:4000/file-bucket/**'),
+            new URL('https://landrup-dans-api-ghwx.onrender.com/file-bucket/**'),
+        ],
     },
 };
 
